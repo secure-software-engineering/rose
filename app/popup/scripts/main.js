f94c2d8e5666ad1ceedca5c511354d6d409f82ac
Oliver Hoffmann <oliverh855@gmail.com>
2013-11-27 13:28:46 +0100

    Added formatted time popup to diary page.

--- app/popup/scripts/main.js
+++ app/popup/scripts/main.js
@@ -25,7 +25,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/* global KangoAPI */
+/* global KangoAPI, kango */
 
 KangoAPI.onReady(function () {
 

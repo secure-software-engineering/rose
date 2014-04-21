5539be2c86c9cf531ed00f777eec0ff61842d6f0
Sebastian Ruhleder <sebastian.ruhleder@googlemail.com>
2014-04-21 12:10:49 +0200

    Plugin jquery.patterns.js now wraps searched node in container
    - Experimental version, similar to the 'real' 0.0.3 of Rose 3

--- app/scripts/jquery.patterns.js
+++ app/scripts/jquery.patterns.js
@@ -1,35 +1,36 @@
 ;(function($) {
   // Sebastian Ruhleder, v. 0.0.2
+  // experimental, Rose-2 customized
   "use strict";
-  
+
   var preprocess = function(structure) {
     for(;;) {
       var match = /\<(.+)\>\{(.+)\}\<(.+)\>/gi.exec(structure);
-      
+
       if(match == null) {
         return structure;
       }
-      
+
       structure = '<' + match[1] + ' content="{' + match[2] + '}"><' + match[3] + '>';
     }
   };
-  
+
   var findPattern = function(pattern, node, events, level) {
     var data = [], hooks = [], success = false;
-    
+
     $(node).find(pattern['tag']).each(function() {
       var fields = {}, currentNode = this, found = true;
-      
+
       var conditionsSatisfied = true;
       for(var i in pattern['conditions']) {
         var condition = pattern['conditions'][i];
-        
+
         if(condition['name'] == "class") {
           var classes = condition['value'].split(" ");
-          
+
           for(var i in classes) {
             var c = classes[i];
-            
+
             if(!$(currentNode).hasClass(c)) {
               conditionsSatisfied = false;
             }
@@ -40,71 +41,71 @@
           }
         }
       }
-      
+
       if(!conditionsSatisfied) {
         return;
       }
-      
+
       $.each(pattern['children'], function(i, child) {
         var rec = findPattern(child, currentNode, events, level + 1);
-        
+
         found &= rec['success'];
-        
+
         var merged = [].concat.apply([], rec['data']);
         for(var i in merged) {
           var set = merged[i];
-          
+
           for(var key in set) {
             fields[key] = set[key];
           }
         }
-        
+
         var merged = [].concat.apply([], rec['hooks']);
         for(var i in merged) {
           var hook = merged[i];
-          
+
           hooks.push(hook);
         }
       });
-      
+
       if(found) {
         success = true;
-        
+
         for(var i in pattern['fields']) {
           var field = pattern['fields'][i];
-          
+
           if(field['name'] == 'content') {
             fields[field['value']] = $(currentNode).html();
           } else {
             fields[field['value']] = $(currentNode).attr(field['name']);
           }
         }
-        
+
         data.push(fields);
-        
+
         for(var i in pattern['events']) {
           var event = pattern['events'][i];
-          
+
           var hook = {
             'node': currentNode,
             'type': event['name'],
             'event': event['value']
           };
-          
+
           hooks.push(hook);
         }
-        
+
         if(level == 0) {
           for(var i in hooks) {
             var hook = hooks[i];
-            
+
             var proceed = true;
             var registeredEvents = $._data(hook['node'], "events");
             if(registeredEvents !== undefined) {
               if(registeredEvents['click'] !== undefined) {
                 for(var j in registeredEvents['click']) {
                   var registeredEvent = registeredEvents['click'][j];
-                  
+
                   if(registeredEvent['data'] !== undefined) {
                     if(registeredEvent['data']['pattern'] !== undefined) {
                       proceed = false;
@@ -113,58 +114,58 @@
                 }
               }
             }
-            
+
             if(!proceed) {
               continue;
             }
-            
+
             $(hook['node']).on(hook['type'], {'pattern': 'yes', 'fields': fields}, function(event) {
               var func = events[hook['event']];
-              
+
               func(event.data['fields']);
             });
           }
         }
       }
     });
-    
+
     return {
       'success': success,
       'data': data,
       'hooks': hooks
     };
   };
-  
+
   var compilePattern = function(structure) {
     var name = structure.prop('tagName');
-    
+
     var fields = [], conditions = [], events = [];
     $.each(structure[0].attributes, function(i, attr) {
       var entry = {
         'name': attr.name,
         'value': attr.value
       };
-      
+
       if(attr.value.match(/^\{.+\}$/gi)) {
         entry['value'] = entry['value'].replace(/[\{\}]/gi, "");
-        
+
         fields.push(entry);
       } else if(attr.value.match(/^\[.+\]$/gi)) {
         entry['value'] = entry['value'].replace(/[\[\]]/gi, "");
-        
+
         events.push(entry);
       } else {
         conditions.push(entry);
       }
     });
-    
+
     var children = [];
     structure.children().each(function() {
       var childPattern = compilePattern($(this))
-      
+
       children.push(childPattern);
     });
-    
+
     return {
       'tag': name,
       'fields': fields,
@@ -173,8 +174,8 @@
       'children': children
     };
   };
-  
+
   $.fn.applyPattern = function(options) {
-    return findPattern(compilePattern($(preprocess(options['structure']))), this, options['events'], 0);
+    return findPattern(compilePattern($(preprocess(options['structure']))), $('<div></div>').append(this.clone()), options['events'], 0);
   };
 })(jQuery);

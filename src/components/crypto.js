a024f8291e1957044548fc0f8794b1fc560f7c47
Sebastian Ruhleder <sebastian.ruhleder@googlemail.com>
2014-07-19 16:31:53 +0200

    Add crypto.js module
    - sha1 function: Generates the SHA1 hash of a message
    - verify function: Verifies a signatures over a message
      and a certificate

---
+++ src/components/crypto.js
@@ -0,0 +1,49 @@
+/** @module crypto */
+
+/* Requirements */
+var jsrsasign = require('jsrsasign');
+
+module.exports = {
+    /**
+     * Generates the SHA1 hash of a message.
+     * @param {String} message - The message to be hashed.
+     * @returns {String}
+     */
+    sha1: function sha1(message) {
+        // Initialize an instance of the MessageDigest class
+        var md = new KJUR.crypto.MessageDigest({
+            alg:  "sha1",
+            prov: "cryptojs"
+        });
+        
+        // Load message
+        md.updateString(message);
+        
+        // Generate and return hash
+        return md.digest();
+    },
+    /**
+     * Verifies the message's claim to authenticity, based on a signature and
+     * a certificate.
+     * @param {String} message - The message whose authenticity shall be verified.
+     * @param {String} signature - The proposed signature of the message.
+     * @param {String} certificate - The certificate with which to verify the signature.
+     * @returns {Boolean}
+     */
+    verify: function verify(message, signature, certificate) {
+        // Initialize an instance of the Signature class
+        var sign = new KJUR.crypto.Signature({
+            alg:  "SHA1withRSA",
+            prov: "cryptojs/jsrsa"
+        });
+        
+        // Load certificate
+        sign.initVerifyByCertificatePEM(certificate);
+        
+        // Load message
+        sign.updateString(message);
+        
+        // Verify and return result
+        return sign.verify(signature);
+    }
+};

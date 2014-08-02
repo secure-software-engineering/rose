a28d155ee80adba0680f77d4d92489813ee5a82f
Sebastian Ruhleder <sebastian.ruhleder@googlemail.com>
2014-08-02 12:46:38 +0200

    Structure configuration file

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,38 @@
-var config = {
+/** @module config */
+
+/** Configuration */
+var _config = {
+  /*************************************
+   * Security                          *
+   *************************************/
+  
+  /**
+   * Salt used for hashing.
+   */
+  salt: "ROSE",
+  
+  /*************************************
+   * Updates                           *
+   *************************************/
+  
+  /**
+   * URL of central repository.
+   */
+  repository: "http://-/-/repository.json",
+  
+  /**
+   * Certificate.
+   */
+  certificate: null,
+  
+  /**
+   * Interval in which ROSE synchronizes with the repository.
+   *
+   * Default value: 2 hours
+   */
+  update_interval: 2 * 60 * 1000
 };
 
-module.exports = config;
+module.exports = function config(item) {
+  return _config[item];
+};

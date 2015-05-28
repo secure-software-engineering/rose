ba542ebd94276b1e479ef6acf2dab0a0d5af3fbc
Oliver Hoffmann <oliverh855@gmail.com>
2015-05-28 13:12:11 +0200

    Correct indentation.

--- app/content_app1.js
+++ app/content_app1.js
@@ -7,36 +7,36 @@ import SystemConfigModel from 'rose/models/system-config';
   var configs = new SystemConfigModel();
   configs.fetch(); //wait for success
 
-//Check for network
-/**
- * URL identifiers of social networks.
- * FIXME: identifiers should load from settings
- */
-var identifiers = {
-  facebook: 'facebook.com',
-  gplus: 'plus.google.com'
-};
+  //Check for network
+  /**
+   * URL identifiers of social networks.
+   * FIXME: identifiers should load from settings
+   */
+  var identifiers = {
+    facebook: 'facebook.com',
+    gplus: 'plus.google.com'
+  };
 
-// Detect network, if possible
-for (var name in identifiers) {
-  var networkDomain = identifiers[name];
+  // Detect network, if possible
+  for (var name in identifiers) {
+    var networkDomain = identifiers[name];
 
-  if ((new RegExp('^https:\/\/[\w\.\-]*(' + networkDomain.replace(/\./g, '\\$&') + ')$')).test(window.location.origin)) {
+    if ((new RegExp('^https:\/\/[\w\.\-]*(' + networkDomain.replace(/\./g, '\\$&') + ')$')).test(window.location.origin)) {
 
-    /* Observer Engine
-     * ----------------
-     *
-     * Start observer engine.
-     */
-    ObserverEngine.register(name);
+      /* Observer Engine
+       * ----------------
+       *
+       * Start observer engine.
+       */
+      ObserverEngine.register(name);
 
-    if (name === 'facebook' && configs.get('roseCommentsIsEnabled')) {
-      var facebookUI = new FacebookUI();
-      facebookUI.redrawUI();
+      if (name === 'facebook' && configs.get('roseCommentsIsEnabled')) {
+        var facebookUI = new FacebookUI();
+        facebookUI.redrawUI();
+      }
+
+      break;
     }
-
-    break;
   }
-}
 
 })();

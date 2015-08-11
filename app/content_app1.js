28f7c087482353034c69ccda53de32fbe139b03e
Felix A. Epp <work@felixepp.de>
2015-08-11 23:10:16 +0200

    EngageTracker with all data and preliminary condition (needs more testing/tuning)

--- app/content_app1.js
+++ app/content_app1.js
@@ -24,6 +24,7 @@ import 'babelify/polyfill';
 import ObserverEngine from 'rose/observer-engine';
 import FacebookUI from 'rose/facebook-ui';
 import SystemConfigModel from 'rose/models/system-config';
+import NetworkCollection from 'rose/collections/networks';
 
 import ClickTracker from 'rose/activity-trackers/click';
 import MouseMoveTracker from 'rose/activity-trackers/mousemove';
@@ -33,43 +34,41 @@ import FBLoginTracker from 'rose/activity-trackers/facebook-login';
 /* Content Script */
 (function() {
   var configs = new SystemConfigModel();
-  configs.fetch(); //wait for success
+  var networks = new NetworkCollection();
+  configs.fetch({success: () => {
+    networks.fetch({success: () => {
+      networks.find((network) => {
+
+        // Detect network by its domain in the current origin of this page
+        if ((new RegExp('^https:\/\/[\w\.\-]*(' + network.get('identifier').replace(/\./g, '\\$&') + ')$')).test(window.location.origin)) {
+          let networkName = network.get('name');
+
+          /* Observer Engine
+           * ----------------
+           * Start observer engine.
+           */
+          ObserverEngine.register(networkName);
+
+          if (networkName === 'facebook' && configs.get('roseCommentsIsEnabled')) {
+            var facebookUI = new FacebookUI();
+            facebookUI.redrawUI();
+            // setTimeout(facebookUI.startSurvey, 5000, facebookUI);
+          }
+
+          ClickTracker.start();
+          MouseMoveTracker.start();
+          ScrollTracker.start();
+          FBLoginTracker.start();
+
+          return true;
+        }
+      });
+    }});
+  }}); //wait for success
 
   //Check for network
-  /**
-   * URL identifiers of social networks.
-   * FIXME: identifiers should load from settings
-   */
-  var identifiers = {
-    facebook: 'facebook.com',
-    gplus: 'plus.google.com'
-  };
 
-  // Detect network, if possible
-  for (var name in identifiers) {
-    var networkDomain = identifiers[name];
 
-    if ((new RegExp('^https:\/\/[\w\.\-]*(' + networkDomain.replace(/\./g, '\\$&') + ')$')).test(window.location.origin)) {
 
-      /* Observer Engine
-       * ----------------
-       *
-       * Start observer engine.
-       */
-      ObserverEngine.register(name);
-
-      if (name === 'facebook' && configs.get('roseCommentsIsEnabled')) {
-        var facebookUI = new FacebookUI();
-        facebookUI.redrawUI();
-      }
-
-      ClickTracker.start();
-      MouseMoveTracker.start();
-      ScrollTracker.start();
-      FBLoginTracker.start();
-
-      break;
-    }
-  }
 
 })();

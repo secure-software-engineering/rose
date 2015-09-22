98b85a6b357f6dc89980035cb3f7c037632bd3d0
Oliver Hoffmann <oliverh855@gmail.com>
2015-09-22 15:24:28 +0200

    Refactor updater.js to make us of promises and fetch where possible.

--- app/rose/updater.js
+++ app/rose/updater.js
@@ -1,163 +1,175 @@
-import ConfigModel from 'rose/models/system-config';
-import ObserverCollection from 'rose/collections/observers';
-import ExtractorCollection from 'rose/collections/extractors';
-import NetworkCollection from 'rose/collections/networks';
-import verify from 'rose/verify.js';
-import { Promise } from 'rsvp';
+import ConfigModel from 'rose/models/system-config'
+import ObserverCollection from 'rose/collections/observers'
+import ExtractorCollection from 'rose/collections/extractors'
+import NetworkCollection from 'rose/collections/networks'
+import verify from 'rose/verify.js'
+import { Promise } from 'rsvp'
 
-let configs = new ConfigModel();
+let configs = new ConfigModel()
 
-let cancelUpdate = (errorMsg) => {
-  console.log(errorMsg);
-  kango.dispatchMessage('update-result', {status: 'failure', errorMsg});
+let cancelUpdate = (msg) => {
+  console.log(msg)
+  kango.dispatchMessage('update-result', {status: 'failure', msg})
 }
 
 let getVerified = (url, key) => {
-  let baseFile = fetch(url);
-  let baseFileSig = fetch(url + '.asc');
+  let baseFile = fetch(url)
+  let baseFileSig = fetch(url + '.asc')
 
   return Promise.all([baseFile, baseFileSig])
     .then((requests) => {
-      return Promise.all(requests.map((request) => request.text()));
+      return Promise.all(requests.map((request) => request.text()))
     })
     .then((files) => {
       return verify(files[0], files[1], key).then((fingerprint) => {
         if (parseInt(configs.get('fingerprint'), 16) === parseInt(fingerprint, 16)) {
-          return files[0];
+          return files[0]
         } else {
-          return Promise.reject(new Error('Fingerprint does not match.'));
+          return Promise.reject(new Error('Fingerprint does not match.'))
         }
-      });
+      })
     })
 }
 
 let update = () => {
   configs.fetch({success: () => {
-    //request repo url, signature and key
-    //FIX: get repo url from configs
-    $.get(configs.get('repositoryURL') + 'public.key').done((key) => {
+      let repositoryURL = configs.get('repositoryURL')
+      let baseFileUrl = repositoryURL + 'base.json'
+      let publicKeyUrl = repositoryURL + 'public.key'
 
-      getVerified(configs.get('repositoryURL') + configs.get('fileName'),key).then((confData) => {
-        //verification of base config file was successfull
-        if (configs.get('timestamp') >= confData.timestamp) cancelUpdate('All up-to-date');
-        else {
+      fetch(publicKeyUrl)
+        .then((request) => request.text())
+        .then((publicKey) => {
+          return getVerified(baseFileUrl, publicKey)
+            .then((confData) => {
+              confData = JSON.parse(confData);
 
-          let networks = new NetworkCollection();
-          networks.fetch({success: () => {
-            let promises = [];
-            let cnt = 0;
+              if (configs.get('timestamp') >= confData.timestamp) {
+                cancelUpdate('All up-to-date')
+                return []
+              } else {
+                configs.set('timestamp', confData.timestamp)
 
-            // iterate through networks that are configured in Rose
-            networks.each((network) => {
+                return new Promise((resolve, reject) => {
+                  let networks = new NetworkCollection()
+                  networks.fetch({success: () => {
+                      let promises = []
 
-              let remoteNetwork = _.findWhere(confData.networks, {name: network.get('name')});
+                      // iterate through networks that are configured in Rose
+                      networks.each((network) => {
+                        let remoteNetwork = _.findWhere(confData.networks, {name: network.get('name')})
 
-              if (remoteNetwork !== undefined) {
-                if (remoteNetwork.observers !== undefined) {
-                  //request and verify observer file
-                  promises.push(getVerified(confData.url + remoteNetwork.observers, key));
-                }
-                if (remoteNetwork.extractors !== undefined) {
-                  //request and verify extractor file
-                  promises.push(getVerified(confData.url + remoteNetwork.extractors, key));
-                }
+                        if (remoteNetwork !== undefined) {
+                          if (remoteNetwork.observers !== undefined) {
+                            // request and verify observer file
+                            promises.push(getVerified(confData.url + remoteNetwork.observers, publicKey))
+                          }
+                          if (remoteNetwork.extractors !== undefined) {
+                            // request and verify extractor file
+                            promises.push(getVerified(confData.url + remoteNetwork.extractors, publicKey))
+                          }
+                        }
+
+                        resolve(Promise.all(promises))
+                      })
+                  }})
+                })
               }
-            });
+            })
+        })
+        .then((observersExtractors) => {
+          if (observersExtractors.length <= 0) {
+            return Promise.resolve()
+          }
 
-            //when all requested files are verified update configuration
-            Promise.all(promises).then((observersExtractors) => {
-              //Update every network by its observers/extractors
-              (new ObserverCollection()).fetch({success: (observerCollection) => {
+          return new Promise((resolve, reject) => {
+            // Update every network by its observers/extractors
+            (new ObserverCollection()).fetch({success: (observerCollection) => {
                 (new ExtractorCollection()).fetch({success: (extractorCollection) => {
-                  // for collection of observers/extractors update
-                  for (var i = observersExtractors.length - 1; i >= 0; i--) {
-                    if (observersExtractors[i][0].type === 'input' || observersExtractors[i][0].type === 'click') {
-                      cnt += updateByVersion(observerCollection, observersExtractors[i])
-                    }
-                    else {
-                      cnt += updateByVersion(extractorCollection, observersExtractors[i]);
-                    }
-                  }
+                    let updateCounter = 0
 
-                  //Update sucessful: save timestamp and return result
-                  configs.set('timestamp', confData.timestamp);
-                  configs.save();
-                  console.log('Sucessfully updated ' + cnt + 'observers/extractors');
-                  kango.dispatchMessage('update-result', {status: 'success', cnt});
-                }});
-              }});
-            },(error) => {
-              cancelUpdate(error);
-            });
-          }});
-        }
-      }, (error) => {
-        cancelUpdate('Verification of base file failed:' + error);
-      });
-    })
-  }});
-};
+                    // for collection of observers/extractors update
+                    observersExtractors.forEach((list) => {
+                      if (list[0].type === 'input' || list[0].type === 'click') {
+                        updateCounter += updateByVersion(observerCollection, list)
+                      } else {
+                        updateCounter += updateByVersion(extractorCollection, list)
+                      }
+                    })
+
+                    // Update sucessful: save and return result
+                    configs.save()
+                    console.log('Sucessfully updated ' + updateCounter + ' observers/extractors')
+                    kango.dispatchMessage('update-result', {status: 'success', updateCounter})
+
+                    resolve()
+                }})
+            }})
+          })
+        })
+        .catch((error) => {
+          cancelUpdate('Verification of base file failed:' + error)
+        })
+  }})
+}
 
 let compareVersion = (oldVersion, newVersion) => {
+  var result = false
 
-    var result = false;
+  oldVersion = oldVersion.split('.')
+  newVersion = newVersion.split('.')
 
-    oldVersion = oldVersion.split('.');
-    newVersion = newVersion.split('.');
+  for (var i = 0; i < (Math.max(oldVersion.length, newVersion.length)); i++) {
+    if (oldVersion[i] === undefined) { oldVersion[i] = 0; }
+    if (newVersion[i] === undefined) { newVersion[i] = 0; }
 
-    for(var i=0; i < (Math.max(oldVersion.length,newVersion.length)); i++){
-
-        if(oldVersion[i] === undefined){ oldVersion[i] = 0; }
-        if(newVersion[i] === undefined){ newVersion[i] = 0; }
-
-        if(Number(oldVersion[i]) < Number(newVersion[i])){
-            result = true;
-            break;
-        }
-        if(oldVersion[i] !== newVersion[i]) {
-            break;
-        }
+    if (Number(oldVersion[i]) < Number(newVersion[i])) {
+      result = true
+      break
     }
-    return result;
-};
+    if (oldVersion[i] !== newVersion[i]) {
+      break
+    }
+  }
+  return result
+}
 
 let updateByVersion = (collection, newCollection) => {
-  let cnt = 0;
+  let updateCounter = 0
   collection.each((model) => {
-    let remoteModel = _.findWhere(newCollection, {name: model.get('name'), network: model.get('network')});
+    let remoteModel = _.findWhere(newCollection, {name: model.get('name'), network: model.get('network')})
     if (remoteModel !== undefined && compareVersion(model.get('version'), remoteModel.version)) {
-      model.save(remoteModel);
-      cnt++;
+      model.save(remoteModel)
+      updateCounter++
     }
-  });
-  return cnt;
-};
+  })
+  return updateCounter
+}
 
 let load = (networks) => {
-
   (new NetworkCollection()).fetch({success: (networkCol) => {
-    (new ObserverCollection()).fetch({success: (observerCol) => {
-      (new ExtractorCollection()).fetch({success: (extractorCol) => {
-        for (var i = 0; i < networks.length; i++) {
-          if(networks[i].observers !== undefined) {
-            for (var j = 0; j < networks[i].observers.length; j++) {
-              observerCol.create(networks[i].observers[j]);
-            }
-          }
-          if(networks[i].extractors !== undefined) {
-            for (var k = 0; k < networks[i].extractors.length; k++) {
-              extractorCol.create(networks[i].extractors[k]);
-            }
-            //call extractorEngine to start
-          }
+      (new ObserverCollection()).fetch({success: (observerCol) => {
+          (new ExtractorCollection()).fetch({success: (extractorCol) => {
+              for (var i = 0; i < networks.length; i++) {
+                if (networks[i].observers !== undefined) {
+                  for (var j = 0; j < networks[i].observers.length; j++) {
+                    observerCol.create(networks[i].observers[j])
+                  }
+                }
+                if (networks[i].extractors !== undefined) {
+                  for (var k = 0; k < networks[i].extractors.length; k++) {
+                    extractorCol.create(networks[i].extractors[k])
+                  }
+                // call extractorEngine to start
+                }
 
-          delete networks[i].observers;
-          delete networks[i].extractors;
-          networkCol.create(networks[i]);
-        }
-      }});
-    }});
-  }});
-};
+                delete networks[i].observers
+                delete networks[i].extractors
+                networkCol.create(networks[i])
+              }
+          }})
+      }})
+  }})
+}
 
-export default {update, load};
+export default {update, load}

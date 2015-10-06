f48fada95ed3f91c009ba0823bb402cec1bd3791
Oliver Hoffmann <oliverh855@gmail.com>
2015-10-06 18:50:47 +0200

    Fix comment model to show checkboxes array.

--- rui/app/models/comment.js
+++ rui/app/models/comment.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 let model = DS.Model.extend({
   text: DS.attr('string'),
   createdAt: DS.attr('string', { defaultValue: () => (new Date()).toJSON() }),
+  checkbox: DS.attr('array'),
   updatedAt: DS.attr(),
   isPrivate: DS.attr('boolean'),
   rating: DS.attr('array'),

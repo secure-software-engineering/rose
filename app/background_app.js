b1c0dfdb179ac3eb937878c78d0a81ca5a9ee216
Felix Epp <felix.epp@sit.fraunhofer.de>
2016-02-01 18:13:30 +0100

    Bump version to 3.0.3yiran

--- app/background_app.js
+++ app/background_app.js
@@ -49,8 +49,8 @@ import Task from 'rose/task'
   }
 
   const roseVersion = await localforage.getItem('rose-version')
-  if (!roseVersion || roseVersion < '3.0.0') {
-    await localforage.setItem('rose-version', '3.0.0');
+  if (!roseVersion || roseVersion < '3.0.3yiran') {
+    await localforage.setItem('rose-version', '3.0.3yiran');
   }
 
   WindowTracker.start();

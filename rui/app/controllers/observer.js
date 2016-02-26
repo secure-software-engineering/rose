e0fca45af6b783769d9382c243a184ba2f8cd1e6
Oliver Hoffmann <oliverh855@gmail.com>
2016-02-26 16:05:52 +0100

    Adds a simple observer editor to research features panel.

--- rui/app/controllers/observer.js
+++ rui/app/controllers/observer.js
@@ -4,6 +4,21 @@ export default Ember.Controller.extend({
     actions: {
         save() {
             return this.get('model').save()
+        },
+
+        discard() {
+            const observer = this.get('model')
+            observer.rollbackAttributes()
+        },
+
+        addPattern() {
+            const patterns = this.get('model.patterns')
+            patterns.createFragment('pattern')
+        },
+
+        removePattern(pattern) {
+            const patterns = this.get('model.patterns')
+            patterns.removeObject(pattern)
         }
     }
 });
--- rui/app/routes/observers.js
+++ rui/app/routes/observers.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.store.findAll('observer')
+    },
+
+    actions: {
+        addObserver() {
+            const newObserver = this.store.createRecord('observer')
+            this.transitionTo('observer', newObserver)
+        },
+
+        removeObserver(observer) {
+            return observer.destroyRecord()
+        }
     }
 });
